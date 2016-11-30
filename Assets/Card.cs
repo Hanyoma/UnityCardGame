dc0cdd503e990ac3a87a1f08cd030ca84e6ec98a
Anna Hwang <anna.s.hwang@vanderbilt.edu>
2016-11-30 00:17:31 -0600

    hahaha so broken

---
+++ Assets/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Card {
+
+    // Private members
+    private Suit _suit;
+    private int _value;
+
+    // Accessors
+    public Suit suit
+    {
+        get { return _suit; }
+    }
+    public int value
+    {
+        get { return _value; }
+    }
+
+    // Constructors
+    public enum Suit
+    {
+        Heart = 0, Diamond, Club, Spade
+    }
+
+    public Card(Suit s, int val)
+    {
+        _suit = s;
+        _value = val;
+    }
+
+    public Card(Card _rhs)
+    {
+        _suit = _rhs.suit;
+        _value = _rhs.value;
+    }
+
+    public override string ToString()
+    {
+        return "Card: " + _suit + " " + _value;
+    }
+}
