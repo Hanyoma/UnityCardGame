5112eac9fc059a9cfdadbc759cf1a4c4f52ba0bd
Nick Lewis <nlewis2464@gmail.com>
2016-11-29 18:29:00 -0600

    Added card deck, debug button to randomly draw a card

---
+++ Assets/Scripts/CardModel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardModel : MonoBehaviour {
+    SpriteRenderer spriteRenderer;
+
+    public Sprite[] faces;
+    public Sprite cardBack;
+
+    // The index of the current card in the faces array
+    public int cardIndex;
+
+    public void ToggleFace(bool showFace)
+    {
+        if (showFace)
+        {
+            spriteRenderer.sprite = faces[cardIndex];
+        }
+        else
+        {
+            spriteRenderer.sprite = cardBack;
+        }
+    }
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
---
+++ Assets/Scripts/DebugChangeCard.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugChangeCard : MonoBehaviour {
+
+    private CardModel cardModel;
+    private int cardIndex = 0;
+
+    public GameObject card;
+
+
+    // Use this for initialization
+    void Awake () {
+        cardModel = card.GetComponent<CardModel>();
+	}
+	
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 28), "Hit me!"))
+        {
+            cardIndex = Random.Range(0, 52);
+            cardModel.cardIndex = cardIndex;
+            cardModel.ToggleFace(true);
+
+
+            /*
+            if (cardIndex >= cardModel.faces.Length)
+            {
+                cardIndex = 0;
+                cardModel.ToggleFace(false);
+            }
+            else
+            {
+                cardModel.cardIndex = cardIndex;
+                cardModel.ToggleFace(true);
+                cardIndex++;
+            }
+            */
+        }
+    }
+}
