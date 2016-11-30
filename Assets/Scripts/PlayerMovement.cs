c757934df792961823a906853fbf14563b4972ca
Nick Lewis <nlewis2464@gmail.com>
2016-11-30 14:14:11 -0600

    Merge

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        var x = Input.GetAxis("Horizontal") * 0.1f;
+        var z = Input.GetAxis("Vertical") * 0.1f;
+
+        transform.Translate(x, 0, z);
+    }
+}
